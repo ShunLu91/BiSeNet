c551d05290776a0c72372bbd1378de89305016ae
lushun_mac <470651748@qq.com>
2020-02-09 12:03:06 +0800

    workers=0

--- train.py
+++ train.py
@@ -50,7 +50,7 @@ def train():
     ## dataset
     n_classes = 19
     n_img_per_gpu = 4
-    n_workers = 4
+    n_workers = 0
     cropsize = [1024, 1024]
     ds = CityScapes(args.data_dir, cropsize=cropsize, mode='train')
     # sampler = torch.utils.data.distributed.DistributedSampler(ds)
