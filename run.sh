e240d23f08d44a8a9e06ccdefc1a799271e3b6bf
lushun_mac <470651748@qq.com>
2020-02-10 09:51:30 +0800

    max_iter = 100

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-nohup python -u train.py  > ./bisenet.log  2>&1 &
+export CUDA_VISIBLE_DEVICES=5 && nohup python -u train.py \
+--data_dir /home/work/dataset/city/cityscapes/leftImg8bit_trainvaltest  > ./bisenet.log  2>&1 &
--- train.py
+++ train.py
@@ -81,7 +81,7 @@ def train():
     momentum = 0.9
     weight_decay = 5e-4
     lr_start = 1e-2
-    max_iter = 80000
+    max_iter = 100
     power = 0.9
     warmup_steps = 2000
     warmup_start_lr = 1e-5
